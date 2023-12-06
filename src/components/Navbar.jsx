f1c9fb24b13900c7ed38636dfa97c9ee1a229309
Alindo <76451337+alindoNL@users.noreply.github.com>
2023-12-06 14:43:59 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -97,26 +97,7 @@ const Navbar = () => {
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.chatgptgratis.nl/'
-            >
-              ChatGPT
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              className='flex justify-between items-center w-full text-gray-300'
-              href='https://qrcodemaker.nl/'
-            >
-              QrCodeMaker
-            </a>
-          </li>
+         
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
